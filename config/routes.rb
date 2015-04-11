49b15296a69864ff4fe20f8ce2d0f89adb938a22
Abhas Arya <abhasarya@Abhass-MacBook-Pro.local>
2015-04-11 05:24:06 -0400

    add sign in sign out

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 SplitPayments::Application.routes.draw do
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -6,6 +7,8 @@ SplitPayments::Application.routes.draw do
   # root 'welcome#index'
 
   root 'static_pages#index'
+
+  match '/profile', to: 'users#show', via: 'get'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
