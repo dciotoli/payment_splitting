76c93721b9fa437abd17c6baa92d84b5836739ae
Abhas Arya <abhasarya@Abhass-MacBook-Pro.local>
2015-04-10 22:30:24 -0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SplitPayments::Application.config.secret_key_base = '70b1d95bb30de203f22b1705e07a10adcc68674a6c7a4f4fddf0ff363335a4c8ce92476bcec275f4ba90d216e5468b416d6cb2ae72f3a15d6eac607eaac3149b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SplitPayments::Application.config.session_store :cookie_store, key: '_split_payments_session'
